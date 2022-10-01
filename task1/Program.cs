e5794b271f9a655a2180ca0f46c2c6cb7be753b1
Vorodev <cust89@mail.ru>
2022-10-01 12:18:09 +0500

    задача решена

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿Console.WriteLine("Введите первое чило: ");
+int namber1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int namber2 = int.Parse(Console.ReadLine());
+
+if (namber1 > namber2)
+{
+    Console.Write("Наибольшее число: " + namber1);
+} 
+else
+{
+    Console.Write("Наибольшее число: " + namber2);
+}
