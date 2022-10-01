6030e9ba5ced06b38a140047ab96012b16315463
Vorodev <cust89@mail.ru>
2022-10-01 13:58:37 +0500

    решена 2 задача

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите первое чило: ");
+int namber1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе чило: ");
+int namber2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите третье чило: ");
+int namber3 = int.Parse(Console.ReadLine());
+
+int max = namber1;
+
+while (max <namber2){
+    max = namber2;
+}
+while (max <namber3){
+        max = namber3;
+    }
+
+ Console.WriteLine("Наибольшее:" + max);
